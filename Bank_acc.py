049e3a8b5c4d0c1603d894dc9452c4a7a2babbc6
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-10-04 10:59:04 +0530

    initial

---
+++ Bank_acc.py
@@ -0,0 +1,6 @@
+initial_bal=float(input("Enter the initial balance in your bank account: "))
+deposit=float(input("Enter the amount to be deposited: "))
+
+print(f"Initial Balance: {initial_bal:.2f}")
+new_bal=initial_bal+deposit
+print(f"New Balance after deposit of {deposit:.2f}: {new_bal:.2f}")
