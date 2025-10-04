1c40c42fc22b25acb5dd6153555f92127e7a8d8d
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-10-04 11:09:43 +0530

    withdraw

--- Bank_acc.py
+++ Bank_acc.py
@@ -4,3 +4,10 @@ deposit=float(input("Enter the amount to be deposited: "))
 print(f"Initial Balance: {initial_bal:.2f}")
 new_bal=initial_bal+deposit
 print(f"New Balance after deposit of {deposit:.2f}: {new_bal:.2f}")
+
+withdrawal=float(input("Enter the amount to be withdrawn: "))
+if withdrawal>new_bal:
+    print("Insufficient funds for this withdrawal.")   
+else:
+    new_bal-=withdrawal
+    print(f"New Balance after withdrawal of {withdrawal:.2f}: {new_bal:.2f}")
